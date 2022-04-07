33e5557224c377cc29c12ae9e5246e567d24711a
Robert <osipovitchr@gmail.com>
2022-04-07 18:11:15 -0400

    finish todo app / 411HW

--- src/components/App.js
+++ src/components/App.js
@@ -15,14 +15,51 @@ class App extends React.Component {
         this.setState({text: e.target.value})
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        this.setState({ todos: [...this.state.todos, this.state.text], text: '' })
+
+        
+
+
+    }
+
+    handleDelete = (index) => {
+       const filteredTodos = this.state.todos.filter((todo) => {
+            return this.state.todos.indexOf(todo) !== index
+        })
+
+        this.setState({ todos: [...filteredTodos]})
+    }
+
 
     render() {
         console.log(this.state)
         return (
             <main>
-                <button onClick={this.onClickHandler}>change state</button>
-                <input onChange={this.onChangeHandler} />
+                <button onClick={this.onClickHandler}>change isClicked state</button>
+                <p>{this.state.isClicked ? 'True' : 'False'}</p>
+                <br />
+                <br />
+                
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.onChangeHandler} value={this.state.text} />
+                    <button type="submit">Add Todo</button>
+                </form>
 
+                <ol>
+                    {this.state.todos.map((todo, index) => {
+                        return (
+                            <>
+                                {console.log(todo)}
+                                <li key={index}>{todo}</li>
+                                <button onClick={() => this.handleDelete(index) }>Delete</button>
+
+                            </>
+                        )
+                    })}
+                </ol>
             </main>
         )
     }
